9c16e9a4fcdc95ce58872f64bd211a712db9360f
sruthirajan220797 <sruthirajan220797@gmail.com>
2024-11-16 16:20:00 +0530

    test project commit anulal

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sruth
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        
+        int n=5;
+        
+        int s=1;
+        
+        for(int i=1;i<=5;i++){
+            s=s*i;
+        }
+        
+        System.out.println(""+s);
+    }
+}
+
